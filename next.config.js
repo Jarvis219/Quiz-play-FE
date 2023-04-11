9570e0e45ce1f0bdd60c020c1fd6b175d288e85c
Tran Quang <quang.tran@kiaisoft.com>
2023-04-11 13:49:33 +0700

    feat: config domain next image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['https://lh3.googleusercontent.com', 'https://firebasestorage.googleapis.com'],
+  },
 }
 
 module.exports = nextConfig
